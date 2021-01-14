0003f1f6d804cf3eb14593961070cb00a8d8cdb6
jijimy <jijimy123@gmail.com>
2021-01-14 18:38:26 -0500

    mise a jour de l'app

---
+++ src/components/MonAside.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside>
+    <h3>Je suis ton pere</h3>
+    <h5>{{ nombre }}</h5>
+    <button @click="this.ajouter">plus</button>
+    <!-- @ ou v-on:  les 2 ca marche / OU @click="nombre++ (sans methods:{})"-->
+    <button v-on:click="this.diminuer">moins</button>
+
+    <!-- v-model c pour relier les champs et les données -->
+    <input type="number" v-model="nombre" />
+  </aside>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: 0,
+    };
+  },
+  methods: {
+    ajouter() {
+      this.nombre++;
+    },
+    diminuer() {
+      this.nombre--;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/intro.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>bonjour</p>
+    <h1 class="fond" id="yoo">alloooooooooooooo</h1>
+  </div>
+  <span>{{ allo }}</span>
+  <span>{{ compteur }}</span>
+</template>
+
+<style scoped>
+/* scoped c pour ces css n'affeter pas les autres scripts. ONLY les divs dans ce script*/
+  div {
+    background-color: white;
+  }
+  #yoo {
+    color: blue;
+  }
+  
+  .fond {
+    background-color: lightgreen;
+    color: aqua;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      allo : 'bonjour',
+      compteur: 0,
+    }
+  }
+}
+</script>
