6ba1cf9a1e88258944d92f87a28a20eeb0de6303
jijimy <jijimy123@gmail.com>
2021-01-14 19:22:45 -0500

    bonsoir

--- src/components/MonArticle.vue
+++ src/components/MonArticle.vue
@@ -1,32 +1,27 @@
 <template>
   <article>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-      maiores ipsam recusandae dolore. Dolores delectus quia iusto magni
-      molestias impedit enim quidem ullam quo rerum, sapiente alias, quis nulla
-      illo.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem aliquam
-      velit voluptatem eos doloremque similique tenetur, harum quisquam atque
-      dolores? Veritatis optio omnis mollitia, non animi corporis ab harum hic!
-      Sint est sed cupiditate pariatur beatae vitae rem veniam quasi accusamus
-      distinctio tenetur, rerum ipsum commodi, quas voluptate reprehenderit
-      omnis tempora deleniti? Nam deleniti repellat tenetur similique sequi
-      expedita eaque. Animi culpa nisi quam officia similique, in eius
-      recusandae id quae incidunt excepturi eum vitae assumenda eos quis
-      obcaecati? Neque mollitia eius non quibusdam minima. Impedit esse quae
-      voluptates accusantium? Aut, mollitia magni nihil distinctio ex quia
-      officiis quidem incidunt tempora consectetur facilis fugiat laboriosam
-      eaque quasi molestiae assumenda in dolores, voluptates officia libero
-      consequuntur obcaecati?
-    </p>
+    <p v-for="tours in toursBoucle" :key="tours">bonsoir</p>
   </article>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      toursBoucle: 3,
+      commentaires: null,
+    };
+  },
+  beforeCreate() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((reponse) => reponse.json())
+      .then((resultat) => console.log(resultat));
+  },
+};
 </script>
 
-<style scoped>
+<style>
+article {
+  border: 1px solid black;
+}
 </style>
